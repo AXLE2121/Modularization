fcc6927fe065197c841b3fc07866a8b0bcf4742e
AXLE2121 <casioalexchristian21@gmail.com>
2025-10-16 21:30:55 +0800

    Delete eventModule.js

--- eventModule.js
+++
@@ -1,8 +0,0 @@
-export function registerButtonEvent(id, handler) {
-  const btn = document.getElementById(id);
-  if (btn) {
-    btn.addEventListener('click', handler);
-  } else {
-    console.error(`Button with ID "${id}" not found!`);
-  }
-}
